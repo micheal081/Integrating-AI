43c9a80b8c205eb0cc7ac9da5c9753001ef8a7fb
Micheal <michig3000@gmail.com>
2023-07-31 11:09:26 +0100

    added the model name

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ dotenv.config();
 import { Configuration, OpenAIApi } from "openai";
 
 const config = new Configuration({
+  // Add your OPENAI secret key here
   apiKey: process.env.SECRET_KEY,
 });
 
 const openai = new OpenAIApi(config);
+
+const runPrompt = async () => {
+    const prompt = "Tell me a joke about Backend developers";
+
+    const response = await openai.createCompletion({
+        model: "gpt-3.5-turbo-0613"
+    })
+}
